5e7e8ebe999a89d93bc9ce12b32a003835601d56
Alen Alex Ninan <alena@pop-os.localdomain>
2020-06-01 05:42:41 +0530

    init file

---
+++ init.el
@@ -0,0 +1,25 @@
+;;; init.el --- initiation for emacs -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2020 Alen Alex Ninan
+;;
+;; Author: Alen Alex Ninan <http://github/alena>
+;; Maintainer: Alen Alex Ninan <alenalexninan@gmail.com>
+;; Created: June 01, 2020
+;; Modified: June 01, 2020
+;; Version: 0.0.1
+;; Keywords:
+;; Homepage: https://github.com/alena/init
+;; Package-Requires: ((emacs 26.3) (cl-lib "0.5"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;
+;;
+;;; Code:
+
+
+
+(provide 'init)
+;;; init.el ends here
